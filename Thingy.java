943576dc7c3fe8ca07071184d22fe6f1508b9a5e
sky121 <skylarooz@gmail.com>
2015-10-21 07:48:24 -0700

    Update Thingy.java

--- Thingy.java
+++ Thingy.java
@@ -5,8 +5,8 @@ class Thingy
   {
     myX = myY = 0;
   }
-  public int getX(){/* your java code */}
-  public int getY(){/* your java code */}
-  public void setX(int x){/* your java code */}
-  public void setY(int y){/* your java code */}
+  public int getX(){return myX}
+  public int getY(){return myY}
+  public void setX(int x){myX=x}
+  public void setY(int y){myY=y}
 }
